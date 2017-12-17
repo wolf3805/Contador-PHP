68a6666c6f4e53b53db47749dd36621222121a71
Alan <alan3805@gmail.com>
2017-12-17 01:00:27 -0600

    Prueba de carga, contador php

---
+++ Contador.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if (isset($_COOKIE['contador'])) {
+	setcookie('contador', $_COOKIE['contador']+1,time()+60);
+	echo "Numero de visitas: ".$_COOKIE['contador'];
+}
+else
+{
+	setcookie('contador',1,time()+60);
+	echo "Bienvenido al navegador por primera vez";
+}
+
+
+
+?>
